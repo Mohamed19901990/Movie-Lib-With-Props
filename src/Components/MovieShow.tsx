16adad43a7a3531f902f6b4a0d96e6a0c7c7b171
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2023-03-02 22:35:50 +0100

    second commit

--- src/Components/MovieShow.tsx
+++ src/Components/MovieShow.tsx
@@ -26,49 +26,116 @@ const MovieShow = ({
 
   if (toggleMovie === true)
     return (
-      <div>
-        <div>
-          MovieCard List
-          {moviesFiltred.map((value: any, index: any) => (
-            <span key={index}>
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Typography sx={{ mb: 1 }} variant="body2">
-                    Title&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
-                    {value.title}
-                  </Typography>
-                  <Typography sx={{ mb: 1 }} variant="body2">
-                    Description &nbsp;:{value.description}
-                  </Typography>
-                  <Typography sx={{ mb: 1 }} variant="body2">
-                    PosterURL&nbsp;&nbsp;&nbsp;:{value.posterURL}
-                  </Typography>
-                  <Typography variant="body2">
-                    Rating&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
-                    {value.rating}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    variant="contained"
-                    id={value.id}
-                    onClick={remove}
-                    size="small"
-                  >
-                    Remove
-                  </Button>
-                </CardActions>
-              </Card>
-            </span>
-          ))}
-        </div>
-        <div>
-          Movies List
-          {moviesFiltred.map((value: any) => (
-            <p>{value.title}</p>
-          ))}
-        </div>
-      </div>
+      <Box
+        style={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          style={{
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              justifyContent: "center",
+              display: "02marflex",
+            }}
+          >
+            MovieCard List
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              mt: 4.5,
+            }}
+          >
+            {moviesFiltred.map((value: any, index: any) => (
+              <span key={index}>
+                <Card
+                  sx={{
+                    width: 275,
+                    ml: 0.5,
+                    mb: 0.5,
+                    mt: 0.5,
+                    mr: 0.5,
+                    backgroundColor: "black",
+                    color: "white",
+                    borderRadius: "5%",
+                  }}
+                  variant="outlined"
+                >
+                  <CardContent>
+                    <Typography sx={{ mb: 1 }} variant="body2">
+                      Title&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                      &nbsp;{value.title}
+                    </Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">
+                      Description &nbsp;:&nbsp;&nbsp;{value.description}
+                    </Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">
+                      PosterURL&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;{value.posterURL}
+                    </Typography>
+                    <Typography variant="body2">
+                      Rating&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                      &nbsp;{value.rating}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      variant="contained"
+                      id={value.id}
+                      onClick={remove}
+                      size="small"
+                      sx={{
+                        height: "26px",
+                        "&:hover": { backgroundColor: "#d33e3e8d" },
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </CardActions>
+                </Card>
+              </span>
+            ))}
+          </Box>
+        </Box>
+        <Box
+          style={{
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            style={{
+              justifyContent: "center",
+              alignItems: "flex-start",
+              display: "flex",
+              width: "100px",
+            }}
+          >
+            Movies List
+          </Box>
+          <Box
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              width: "100px",
+              flexDirection: "column",
+              mt: 5,
+              backgroundColor: "lightblue",
+            }}
+          >
+            {moviesFiltred.map((value: any) => (
+              <p>{value.title}</p>
+            ))}
+          </Box>
+        </Box>
+      </Box>
     );
 
   return (

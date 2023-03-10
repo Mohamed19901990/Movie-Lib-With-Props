ea80fd455d667acbdb78e97ccdada832e2a630c8
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2023-03-10 11:46:13 +0100

    login version with Google-Identity-Authentification

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { Firestore } from 'firebase/firestore';
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import Copyright from './Components/Copyright';
 import MovieFilter from './Components/MovieFilter';
 import MovieInput from './Components/MovieInput';
@@ -13,9 +13,49 @@ import MovieShow from './Components/MovieShow';
 import useMovies from './Components/useMovies';
 import firebase from './firebase';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import GoogleLoginButton from './Components/ButtonG';
+import { GoogleAuthenticationProvider } from '@cpl/google-identity';
 
 export const moviesContext = createContext([]);
 export const moviesFiltredContext = createContext([]);
+
+const GoogleLogin = () => {
+  useEffect(() => {
+    // oooo();
+
+    google.accounts.id.initialize({
+      client_id:
+        '544306654076-jd6f8ubn4bdau9pmujqbum23pprh9r4a.apps.googleusercontent.com',
+    });
+    google.accounts.id.renderButton(document.getElementById('test') as any, {
+      // size: 'large',
+      // theme: 'filled_black',
+      // type: 'icon',
+      // shape: 'pill',
+      click_listener: onClickHandler,
+    });
+
+    function onClickHandler() {
+      console.log('Sign in with Google button clicked...');
+    }
+  }, []);
+
+  return <div id='test' />;
+};
+
+// const GoogleLogin = () => {
+//   const hallo = google.accounts.id.renderButton(
+//     document.getElementById('test') as any,
+//     {
+//       theme: 'outline',
+//       size: 'large',
+//       text: 'continue_with',
+//       type: 'standard',
+//     },
+//   );
+
+//   return <div id='test' />;
+// };
 
 function App() {
   const {
@@ -27,24 +67,18 @@ function App() {
     clearHandler,
     oooo,
   } = useMovies();
+  // const auth = getAuth();
 
-  window.onload = () => {
-    oooo();
-  };
-
-  const auth = getAuth();
-
-  const googleLogin = () => {
-    console.log('Hello from Button');
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        console.log(user, '======= USER ======');
-      })
-      .catch(console.log);
-  };
-
+  // const googleLogin = () => {
+  //   console.log('Hello from Button');
+  //   const provider = new GoogleAuthProvider();
+  //   signInWithPopup(auth, provider)
+  //     .then((result) => {
+  //       const user = result.user;
+  //       console.log(user, '======= USER ======');
+  //     })
+  //     .catch(console.log);
+  // };
   console.log(movies, 'from app');
 
   return (
@@ -58,9 +92,31 @@ function App() {
         }}
       >
         <Box>MOFLIX : Welcome To Your Movie Library&nbsp;&nbsp;&nbsp;</Box>
-        <Button onClick={googleLogin} variant='contained'>
-          Please LogIn
-        </Button>
+        {/* <Box
+          sx={{
+            display: 'inline-row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          mt={1}
+          ml={50}
+        >
+          <GoogleLoginButton />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            mt={1}
+          >
+            <GoogleLogin /> */}
+            <GoogleAuthenticationProvider clientId='544306654076-jd6f8ubn4bdau9pmujqbum23pprh9r4a.apps.googleusercontent.com'>
+              <GoogleLoginButton />
+            </GoogleAuthenticationProvider>
+          {/* </Box> */}
+        {/* </Box> */}
+        {/* <Button variant='contained'>Please LogIn</Button> */}
       </Box>
       <Box sx={{ display: 'flex', height: '1000px' }} mt={1}>
         <Box sx={{ mt: 8 }}>
